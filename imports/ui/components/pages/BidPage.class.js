3070a69a7e095ee7e2201e9a8b137190a3d27849
jaymontana <nasakthe@gmail.com>
2017-12-25 04:19:58 +0000

    Canvas image crop

--- imports/ui/components/pages/BidPage.class.js
+++ imports/ui/components/pages/BidPage.class.js
@@ -1,7 +1,90 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
-// import MainLogo from '../../media/logo.svg';
+
+class CropRobloxImage extends Component {
+  render() {
+    var img = new Image(),
+    $canvas = $("<canvas>"),
+    canvas = $canvas[0],
+    context;
+    
+    var removeBlanks = function (imgWidth, imgHeight) {
+    var imageData = context.getImageData(0, 0, imgWidth, imgHeight),
+        data = imageData.data,
+        getRBG = function(x, y) {
+            var offset = imgWidth * y + x;
+            return {
+                red:     data[offset * 4],
+                green:   data[offset * 4 + 1],
+                blue:    data[offset * 4 + 2],
+                opacity: data[offset * 4 + 3]
+            };
+        },
+        isWhite = function (rgb) {
+            return (rgb.red > 200 && rgb.green > 200 && rgb.blue > 200) || rgb.opacity < 10;
+        },
+        scanY = function (fromTop) {
+            var offset = fromTop ? 1 : -1;
+            for(var y = fromTop ? 0 : imgHeight - 1; fromTop ? (y < imgHeight) : (y > -1); y += offset) {
+                for(var x = 0; x < imgWidth; x++) {
+                    var rgb = getRBG(x, y);
+                    if (!isWhite(rgb)) {
+                        return y;                        
+                    }      
+                }
+            }
+            return null; 
+        },
+        scanX = function (fromLeft) {
+            var offset = fromLeft? 1 : -1;
+            for(var x = fromLeft ? 0 : imgWidth - 1; fromLeft ? (x < imgWidth) : (x > -1); x += offset) {
+                for(var y = 0; y < imgHeight; y++) {
+                    var rgb = getRBG(x, y);
+                    if (!isWhite(rgb)) {
+                        return x;                        
+                    }      
+                }
+            }
+            return null; 
+        };
+    
+    var cropTop = scanY(true),
+        cropBottom = scanY(false),
+        cropLeft = scanX(true),
+        cropRight = scanX(false),
+        cropWidth = cropRight - cropLeft,
+        cropHeight = cropBottom - cropTop;
+    
+    var $croppedCanvas = $("<canvas>").attr({ width: cropWidth, height: cropHeight });
+    $croppedCanvas[0].getContext("2d").drawImage(canvas,
+        cropLeft, cropTop, cropWidth, cropHeight,
+        0, 0, cropWidth, cropHeight);
+    
+    $(".rbxImage").append($croppedCanvas);
+    };
+    
+    img.crossOrigin = "anonymous";
+    img.onload = function () {
+    $canvas.attr({ width: this.width, height: this.height });
+    context = canvas.getContext("2d");
+    if (context) {
+        context.drawImage(this, 0, 0);
+        removeBlanks(this.width, this.height);
+    } else {
+        alert('Get a real browser!');
+    }
+    }
+    
+    // define here an image from your domain
+    img.src = 'https://t3.rbxcdn.com/60561ec5367320937b1ec7121183f669';
+    return(
+      <div className="rbxImage">
+
+      </div>
+    )
+  }
+}
 
 class IndividualListing extends Component {
   render() {
@@ -16,12 +99,13 @@ class IndividualListing extends Component {
         <div className={this.props.isStacked ? 'isStackedSwitch' : ''}> 
           <div>
             <div className="listingImageContainer">
-              <img src="https://t1.rbxcdn.com/464aa884cad7d042caf65d3b209baae1"/>
+              <CropRobloxImage/>
+              {/* <img src="https://t2.rbxcdn.com/859236863329da321320019e08d2d338"/> */}
             </div>
             <div className="listingBidsPriceContainer">
                 <div className="priceAndTimeContainer">
                   <div> 
-                    <i className="ion"/><span className="actualTimer almostEnded">00:00:27</span>
+                    <i className="ion"/><span className="actualTimer">00:00:27</span>
                   </div>
                 </div>
             </div>
@@ -69,7 +153,7 @@ class BidPage extends Component {
 
   render() {
     const listings = [];
-    const numOfListings = 6;
+    const numOfListings = 1;
     for (var i = 0; i < numOfListings; i++) {
       listings.push(<IndividualListing key={i} {...this.state}/>);
     }
